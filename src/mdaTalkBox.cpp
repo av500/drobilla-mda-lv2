6a4977727f664544e797766223db9a1176f11b37
David Robillard <d@drobilla.net>
2020-11-11 22:17:43 +0100

    Fix potential use of uninitialized value

--- src/mdaTalkBox.cpp
+++ src/mdaTalkBox.cpp
@@ -350,6 +350,8 @@ void mdaTalkBox::lpc(float *buf, float *car, int32_t n, int32_t o)
   float z[ORD_MAX], r[ORD_MAX], k[ORD_MAX], G, x;
   int32_t i, j, nn=n;
 
+  r[0] = 0.0f;
+
   for(j=0; j<=o; j++, nn--)  //buf[] is already emphasized and windowed
   {
     z[j] = r[j] = 0.0f;
